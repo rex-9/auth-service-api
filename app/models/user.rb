7869535b5d8c75dc152b251b2ef116ddbfe53f01
htetwaiyan <htetwaiyan7191@gmail.com>
2024-09-29 12:49:01 +0630

    Add validations rule for user email, and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   #  :lockable, :timeoutable, :trackable and :omniauthable
   # has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
   validates :password_confirmation, presence: true, if: -> { (new_record? || !password.nil?) && provider != "google" }
-  validates :name, length: { maximum: 50 }, allow_blank: true
+  validates :name, length: { maximum: 50 }, format: { without: /[<>:;?]/ }, allow_blank: true
   validate :photo_must_be_a_valid_url, if: -> { photo.present? }
 
   private
