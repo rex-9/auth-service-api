dc7b7440c731f4f9fea2984676da0e4f2e1f633e
htetwaiyan <htetwaiyan7191@gmail.com>
2024-09-29 12:49:49 +0630

    Write tests using shoulda-matchers for user model, Update user factories using faker GEM

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
     name { "Test User" }
-    email { "test@example.com" }
+    email { Faker::Internet.unique.email }
     password { "password" }
     password_confirmation { "password" }
     provider { "email" }
+    photo { 'https://www.google.com/' }
   end
 end
